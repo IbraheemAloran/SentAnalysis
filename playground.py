527b9bd115966304caceb4858f0d35818f35a629
Ibraheem Aloran <ibraheemaloran@gmail.com>
2024-04-29 15:36:05 -0400

    tagging text

--- playground.py
+++ playground.py
@@ -1,21 +1,40 @@
 #test methods, funtions and algorithms
-
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.corpus import stopwords
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize, PunktSentenceTokenizer
+from nltk.corpus import stopwords, state_union
 from nltk.stem import PorterStemmer
 
 text = "Hello Mrs. Doe, hope everything is well. I am excited to learn Python. I am nervous about the project."
 stopwrds = set(stopwords.words("english"))
 words = word_tokenize(text)
 filteredSent = []
+trainText = state_union.raw("2005-GWBush.txt")
+samplText = state_union.raw("2006-GWBush.txt")
 
-ps = PorterStemmer()
+cusTok = PunktSentenceTokenizer(trainText)
+tokenz = cusTok.tokenize(samplText)
+
+
+def tagFile():
+    try:
+        for i in tokenz:
+            words = word_tokenize(i)
+            tags = nltk.pos_tag(words)
+            print(tags)
+    except Exception as e:
+        print(str(e))
+
+tagFile()
 
 
 
-for w in words:
-    if w not in stopwrds:
-        filteredSent.append(w)
+#ps = PorterStemmer()
+
+
+
+#for w in words:
+#    if w not in stopwrds:
+#        filteredSent.append(w)
 
 #print(sent_tokenize(text))
 
@@ -23,4 +42,3 @@ for w in words:
 
 
 
-print(filteredSent)
