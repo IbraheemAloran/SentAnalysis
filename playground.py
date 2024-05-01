c0db098d0b7225606a77332cc7503d4789788450
Ibraheem Aloran <ibraheemaloran@gmail.com>
2024-04-30 23:51:20 -0400

    Added word similarity, synsets, and lemmas

--- playground.py
+++ playground.py
@@ -1,7 +1,7 @@
 #test methods, funtions and algorithms
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize, PunktSentenceTokenizer
-from nltk.corpus import stopwords, state_union
+from nltk.corpus import stopwords, state_union, abc, movie_reviews, wordnet
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 
 text = "Hello Mrs. Doe, hope everything is well. I am excited to learn Python. I am nervous about the project."
@@ -11,14 +11,25 @@ filteredSent = []
 trainText = state_union.raw("2005-GWBush.txt")
 samplText = state_union.raw("2006-GWBush.txt")
 
+
 cusTok = PunktSentenceTokenizer(trainText)
 tokenz = cusTok.tokenize(samplText)
 
-
+###LEMMA
 lem = WordNetLemmatizer()
-print(lem.lemmatize("geese"))
-print(lem.lemmatize("mocking", pos="v"))
-print(lem.lemmatize("better", pos="a"))
+# print(lem.lemmatize("geese"))
+# print(lem.lemmatize("mocking", pos="v"))
+# print(lem.lemmatize("better", pos="a"))
+#
+#
+# ###TOKEIZE TEXT FILES
+# text = abc.raw("science.txt")
+# tok = sent_tokenize(text)
+# print(tok[5:10])
+#
+# text = movie_reviews.raw("neg\cv000_29416.txt")
+# tok = sent_tokenize(text)
+# print(tok[5:10])
 
 def tagFile():
     try:
@@ -50,6 +61,42 @@ def tagFile():
 #ps = PorterStemmer()
 
 
+####SYNONYM SETS
+# syn = wordnet.synsets("plan")
+# print(syn)
+#
+# print(syn[0].name())
+# print(syn[0].lemmas()[0].name())
+# print(syn[0].definition())
+# print(syn[0].examples())
+
+syns = []
+ants = []
+
+
+####SYNONYMS AND ANTONYMS
+for syn in wordnet.synsets("good"):
+    for lem in syn.lemmas():
+        # print(lem)
+        syns.append(lem.name())
+        if lem.antonyms():
+            ants.append(lem.antonyms()[0].name())
+
+
+# print(set(syns))
+# print(set(ants))
+#####WORD SIMILARITY
+w1 = wordnet.synsets("ship")[0]
+w2 = wordnet.synsets("boat")[0]
+print(w1.wup_similarity(w2))
+
+w1 = wordnet.synsets("ship")[0]
+w2 = wordnet.synsets("car")[0]
+print(w1.wup_similarity(w2))
+
+w1 = wordnet.synsets("dog")[0]
+w2 = wordnet.synsets("cat")[0]
+print(w1.wup_similarity(w2))
 
 #for w in words:
 #    if w not in stopwrds:
